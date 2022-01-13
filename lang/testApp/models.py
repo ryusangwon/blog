b15864056facc9c9444a431f5b85cdc477cc35bd
ryusangwon <r8uuuuuuuu@gmail.com>
2022-01-13 18:48:44 +0900

    Add Database Schema

--- lang/testApp/models.py
+++ lang/testApp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=20)
+    nation = models.CharField(max_length=20)
+    nation_img = models.CharField(max_length=255)
+    age = models.IntegerField()
+    
+    def __str__(self):
+        return self.name
