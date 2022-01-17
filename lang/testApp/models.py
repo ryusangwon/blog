88b4e7e41526d7e4d26e451c8d873306b08d8b45
ryusangwon <r8uuuuuuuu@gmail.com>
2022-01-18 00:14:51 +0900

    Connect details

--- lang/testApp/models.py
+++ lang/testApp/models.py
@@ -6,6 +6,6 @@ class User(models.Model):
     nation = models.CharField(max_length=20)
     nation_img = models.CharField(max_length=255)
     age = models.IntegerField()
-    
+
     def __str__(self):
         return self.name
--- lang/testApp/urls.py
+++ lang/testApp/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('user/', views.index),
+    path('user/', views.index, name='index'),
     path('user/<int:pk>/', views.word_detail, name='word-detail'),
 ]
