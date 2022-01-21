f1890625ef0ea1e84aeff6c4608b94a54d6efac0
ryusangwon <r8uuuuuuuu@gmail.com>
2022-01-21 22:57:19 +0900

    Modify forms

--- lang/testApp/forms.py
+++ lang/testApp/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 from .models import User
+from .validators import validate_test
+from django.core.exceptions import ValidationError
 
 class UserForm(forms.ModelForm):
     
@@ -7,3 +9,6 @@ class UserForm(forms.ModelForm):
         model = User
         #fields = '__all__'
         fields = ['name', 'nation', 'age']
+        widgets = {'name':forms.TextInput(attrs={'class':'name', 'placeholder':'이름을 입력하세요'}),
+                   'nation':forms.Textarea(attrs={'국적을 입력하세요'}),
+                   'age':forms.TextInput(attrs={'placeholder':'나이를 입력하세요'})}
